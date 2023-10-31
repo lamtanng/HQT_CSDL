6496bac1beb76fd49e1335616f88e49b3a75f1b2
Wayy8T <waytnt17523@gmail.com>
2023-11-01 00:31:06 +0700

    cach

--- DemoDoAn/DemoDoAn/Program.cs
+++ DemoDoAn/DemoDoAn/Program.cs
@@ -16,6 +16,8 @@ namespace DemoDoAn
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+
             Application.Run(new Login());
         }
     }
